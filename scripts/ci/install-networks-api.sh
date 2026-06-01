3462eae18ea3b3060f7ed56b908d025224d67020
JackStar6677-1 <pablo.elias.miranda.292003@gmail.com>
2026-05-31 22:51:10 -0400

    fix: actualiza Networks a drake 6

--- scripts/ci/install-networks-api.sh
+++ scripts/ci/install-networks-api.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-readonly NETWORKS_VERSION="11.0.0-drake.5"
-readonly NETWORKS_SHA256="3aa4358f37d52bbbea9ce48fe9371e9c606286606d9c905ba6634207e3c6b957"
+readonly NETWORKS_VERSION="11.0.0-drake.6"
+readonly NETWORKS_SHA256="2029ceeab23adcea30a9f9201f2d11e3803b277f252d15efef6f2d6f2dc11d77"
 readonly NETWORKS_URL="https://github.com/DrakesCraft-Labs/NetworksV6-drake/releases/download/v${NETWORKS_VERSION}/NetworksV6-Drake-v${NETWORKS_VERSION}.jar"
 readonly TARGET_DIR="${RUNNER_TEMP:-${TMPDIR:-/tmp}}/drakes-networks-api"
 readonly TARGET_JAR="${TARGET_DIR}/NetworksV6-Drake-v${NETWORKS_VERSION}.jar"
